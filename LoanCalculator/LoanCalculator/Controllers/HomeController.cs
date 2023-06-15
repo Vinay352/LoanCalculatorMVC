8c305b40e4a0fe755c1df54fe9d017996441b5df
Vinay352 <vinay.j3097@gmail.com>
2023-06-15 14:59:10 +0530

    Loan Calculator MVC
    Backend perfectly working

---
+++ LoanCalculator/LoanCalculator/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LoanCalculator.Models;
+using LoanCalculator.Helpers;
+
+namespace LoanCalculator.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public ActionResult App()
+    {
+        Loan loan = new Loan();
+
+        loan.Amount = 1300m;
+        loan.Rate = 3.1m;
+        loan.Term = 60;
+        loan.Payment = 0.0m;
+        loan.TotalInterest = 0.0m;
+        loan.TotalCost = 0.0m;
+
+        return View(loan);
+    }
+
+    [HttpPost]
+    [ActionName("App")]
+    [AutoValidateAntiforgeryToken]
+    public ActionResult App(Loan loan)
+    {
+        // Calculate the loan and get the payments
+        var loanHelper = new LoanHelper();
+
+        Loan newLoan = loanHelper.GetPayments(loan);
+
+        return View(newLoan);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ LoanCalculator/LoanCalculator/Models/LoanPayment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LoanCalculator.Models
+{
+	public class LoanPayment
+	{
+
+		public int Month { get; set; }
+
+		//[DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
+		public decimal Payment { get; set; }
+
+		public decimal MonthlyPrincipal { get; set; }
+
+		public decimal MonthlyInterest { get; set; }
+
+		public decimal TotalInterest { get; set; }
+
+		public decimal Balance { get; set; }
+	}
+}
+
